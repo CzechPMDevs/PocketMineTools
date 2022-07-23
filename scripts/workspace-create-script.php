c04d0664fbbb27c12589f1dd239f4af1d99ba8ba
VixikHD <dev@vixikhd.me>
2022-07-23 20:29:30 +0200

    Rebrand script

--- scripts/workspace-create-script.php
+++ scripts/workspace-create-script.php
@@ -1,8 +1,26 @@
 <?php
 
+/**
+ * PocketMine Tools - Tools to simplify PocketMine plugin development
+ * Copyright (C) 2022 CzechPMDevs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 declare(strict_types=1);
 
-namespace vixikhd\bootstrap {
+namespace czechpmdevs\pocketminetools {
 	use function exec;
 	use function fgets;
 	use function file_get_contents;
